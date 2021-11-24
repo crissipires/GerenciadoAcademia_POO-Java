69c3df7fe3cbc5c0db87495e36d76ab0aa5f294d
crissipires <crissiano_pire@hotmail.com>
2021-11-24 13:33:03 -0300

    :sparkles: add domain

---
+++ src/br/ucsal/poo/ted/Main.java
@@ -0,0 +1,9 @@
+package br.ucsal.poo.ted;
+
+import br.ucsal.poo.ted.presentation.MenuTUI;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuTUI.executar();
+    }
+}
